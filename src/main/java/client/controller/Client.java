e1178555dcff10acd9d3b9bc4660c81e2ef5a4e2
karstensgit <karstenschaefers@gmx.net>
2016-12-08 16:04:48 +0100

    Merge branch 'develop' of https://github.com/lukro/bib2html-text into develop
    # Conflicts:
    #	src/main/java/server/modules/PartialResultCollector.java

--- src/main/java/client/controller/Client.java
+++ src/main/java/client/controller/Client.java
@@ -1,32 +1,48 @@
 package client.controller;
 
-import com.rabbitmq.client.AMQP;
-import com.rabbitmq.client.Consumer;
-import com.rabbitmq.client.Envelope;
-import com.rabbitmq.client.ShutdownSignalException;
+import com.rabbitmq.client.*;
 import client.model.ClientFileModel;
+import global.controller.IConnectionPoint;
 import global.model.DefaultClientRequest;
 import org.apache.commons.lang3.SerializationUtils;
 
 import java.io.IOException;
+import java.sql.Time;
 import java.util.UUID;
 import java.util.concurrent.TimeoutException;
 
 /**
  * Created by daan on 11/30/16.
  */
-public class Client extends ConnectionPoint implements Runnable, Consumer {
+public class Client implements Runnable, Consumer, IConnectionPoint {
 
-    private final String clientID = UUID.randomUUID().toString();
+    private final String clientID;
     private final BibTeXEntryFormatter bibTeXEntryFormatter = BibTeXEntryFormatter.getINSTANCE();
-    private ClientFileModel clientFileModel = new ClientFileModel(clientID);
+    private ClientFileModel clientFileModel;
+    private final Connection connection;
+    private final Channel channel;
+    private final String hostIP;
+    private final String callbackQueueName;
+    private String outputDirectory;
+    private final String CLIENT_REQUEST_NAME = "clientRequestQueue";
 
     public Client() throws IOException, TimeoutException {
-        super();
+        this("localhost");
     }
 
     public Client(String hostIP) throws IOException, TimeoutException {
-        super(hostIP);
+        this(hostIP, UUID.randomUUID().toString());
+    }
+
+    public Client(String hostIP, String callbackQueueName) throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost(hostIP);
+        this.hostIP = hostIP;
+        this.clientID = callbackQueueName;
+        this.callbackQueueName = callbackQueueName;
+        this.connection = factory.newConnection();
+        this.channel = connection.createChannel();
+        this.clientFileModel = new ClientFileModel(clientID);
     }
 
     @Override
@@ -40,7 +56,7 @@ public class Client extends ConnectionPoint implements Runnable, Consumer {
 
     public void sendClientRequest() throws IOException {
         System.out.println("request sent");
-        channel.basicPublish("", QUEUE_TO_SERVER_NAME, null, SerializationUtils.serialize(this.createClientRequest()));
+        channel.basicPublish("", CLIENT_REQUEST_NAME, null, SerializationUtils.serialize(this.createClientRequest()));
     }
 
     public void sendMessage(String message, String routingKey) throws IOException {
@@ -74,7 +90,7 @@ public class Client extends ConnectionPoint implements Runnable, Consumer {
 
     @Override
     public void handleDelivery(String s, Envelope envelope, AMQP.BasicProperties basicProperties, byte[] bytes) throws IOException {
-        System.out.println(this.routingKey + ": message received");
+        System.out.println("Client: message received");
     }
 
     public String getClientID() {
@@ -91,5 +107,25 @@ public class Client extends ConnectionPoint implements Runnable, Consumer {
 
     public DefaultClientRequest createClientRequest() throws IOException {
         return new DefaultClientRequest(this.clientID, bibTeXEntryFormatter.createBibTeXEntryObjectListFromClientFileModel(this.clientFileModel));
+    }
+
+    @Override
+    public void closeConnection() {
+
+    }
+
+    @Override
+    public void initConnectionPoint() {
+
+    }
+
+    @Override
+    public String getHostIP() {
+        return null;
+    }
+
+    @Override
+    public String getID() {
+        return null;
     }
 }
