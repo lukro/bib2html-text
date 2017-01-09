418444168d909713efa0a3af7a135ebe4fa24202
daan <dom1do3d3l@gmx.de>
2017-01-09 08:00:27 +0100

    quick save

--- src/main/java/microservice/model/processor/DefaultEntryProcessor.java
+++ src/main/java/microservice/model/processor/DefaultEntryProcessor.java
@@ -2,6 +2,7 @@ package microservice.model.processor;
 
 import global.identifiers.EntryIdentifier;
 import global.identifiers.FileType;
+import global.logging.Log;
 import global.model.IEntry;
 import global.model.IPartialResult;
 import microservice.model.validator.CSLDummyValidator;
@@ -9,6 +10,8 @@ import microservice.model.validator.TemplateValidator;
 import microservice.model.validator.IValidator;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.*;
 
 
@@ -21,18 +24,9 @@ public class DefaultEntryProcessor implements IEntryProcessor {
     private static final IValidator<String> CSL_VALIDATOR = new CSLDummyValidator();
     private static final IValidator<String> TEMPLATE_VALIDATOR = new TemplateValidator();
 
-    public DefaultEntryProcessor() {
-    }
+    private HashMap<FileType, String> fileIdentifiers;
 
-    private HashMap<FileType, String> createFileIdentifiersFromIEntry(IEntry iEntry) {
-        HashMap<FileType, String> result = new HashMap<>();
-        String hashCode = Integer.toString(Math.abs(iEntry.hashCode()));
-        result.put(FileType.BIB, hashCode + ".bib");
-        result.put(FileType.CSL, hashCode + ".csl");
-        result.put(FileType.TEMPLATE, hashCode + "_template.html");
-        result.put(FileType.MD, hashCode + ".md");
-        result.put(FileType.RESULT, hashCode + "_result.html");
-        return result;
+    public DefaultEntryProcessor() {
     }
 
     private static int pandocDoWork(String cslName, String templateName, String wrapperName, EntryIdentifier entryIdentifier) throws IOException, InterruptedException {
@@ -60,13 +54,65 @@ public class DefaultEntryProcessor implements IEntryProcessor {
 
     @Override
     public List<IPartialResult> processEntry(IEntry toConvert) {
-//        try {
-        return null;
-//            return convertEntry(toConvert);
-//        } catch (InterruptedException e) {
-//            //TODO : Handle Properly.
-//            Log.log("MicroService got interrupted, returned null", e);
-//            return null;
-//        }
+        List<IPartialResult> result = new ArrayList<>();
+        this.fileIdentifiers = createFileIdentifiersFromIEntry(toConvert);
+
+        final String mdString = "--- \nbibliography: " + fileIdentifiers.get(FileType.BIB) + ".bib\nnocite: \"@*\" \n...";
+
+        final ArrayList<String> cslFilesToUse, templatesToUse;
+
+        cslFilesToUse = new ArrayList<>(correctUserFileLists(toConvert.getCslFiles(), FileType.CSL));
+        templatesToUse = new ArrayList<>(correctUserFileLists(toConvert.getTemplates(), FileType.TEMPLATE));
+
+        writeUserFiles(cslFilesToUse, FileType.CSL);
+        writeUserFiles(templatesToUse, FileType.TEMPLATE);
+
+        try {
+            Files.write(Paths.get(fileIdentifiers.get(FileType.BIB)), toConvert.getContent().getBytes());
+            Files.write(Paths.get(fileIdentifiers.get(FileType.MD)), mdString.getBytes());
+
+
+
+        } catch (IOException e) {
+
+
+        } finally {
+
+        }
+
+
+        return result;
     }
+
+    private HashMap<FileType, String> createFileIdentifiersFromIEntry(IEntry iEntry) {
+        HashMap<FileType, String> result = new HashMap<>();
+        String hashCode = Integer.toString(Math.abs(iEntry.hashCode()));
+        result.put(FileType.BIB, hashCode + ".bib");
+        result.put(FileType.CSL, hashCode + ".csl");
+        result.put(FileType.TEMPLATE, hashCode + "_template.html");
+        result.put(FileType.MD, hashCode + ".md");
+        result.put(FileType.RESULT, hashCode + "_result.html");
+        return result;
+    }
+
+    private ArrayList<String> correctUserFileLists(ArrayList<String> fileList, FileType fileType) {
+        if (fileList.size() != 0)
+            return fileList;
+        if (fileType == FileType.CSL)
+            return new ArrayList<>(Arrays.asList("DEFAULT CSL"));
+        return new ArrayList<>(Arrays.asList("DEFAULT TEMPLATE"));
+    }
+
+    private void writeUserFiles(ArrayList<String> fileList, FileType fileType) {
+        String fileName = "";
+        for (int i = 0; i < fileList.size(); i++) {
+            try {
+                fileName = i + "_" + fileIdentifiers.get(fileType);
+                Files.write(Paths.get(fileName), fileList.get(i).getBytes());
+            } catch (IOException e) {
+                Log.log("failed to write file '" + fileName + "' to filesystem.");
+            }
+        }
+    }
+
 }
